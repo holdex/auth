97a18f3d6aaf3eb4691775a27c1cea505d6d9134
Samuel Ralak <thesamuelralak@gmail.com>
2019-11-08 04:36:32 +0300

    feature: Identify user by email
    fleshed out the signin page

---
+++ src/routes/signin/index.svelte
@@ -0,0 +1,57 @@
+<script>
+  import { onMount } from "svelte";
+  let emailInput;
+
+  onMount(function() {
+    emailInput.focus();
+  });
+</script>
+
+<style>
+  .container.auth-container {
+    width: 50%;
+  }
+
+  .form-container {
+    border: 2px solid #c3cfd9;
+    border-radius: 0.35rem;
+    padding: 4rem;
+    margin: 2rem 0;
+  }
+</style>
+
+<svelte:head>
+  <title>Sign in | Holdex</title>
+</svelte:head>
+
+<div class="container auth-container">
+  <a href="/signup" class="u-pull-right">Create account</a>
+  <h5>[Holdex Logo]</h5>
+
+  <div class="form-container">
+    <h5>Sign in</h5>
+    <p>Please enter your email</p>
+    <form>
+      <label for="email">Email</label>
+      <input
+        class="u-full-width"
+        type="email"
+        id="email"
+        bind:this={emailInput}
+        required />
+      <div class="text-center">
+        <button class="button-primary" type="submit">Next</button>
+      </div>
+
+    </form>
+  </div>
+
+  <div class="text-center">
+    <p>
+      Welcome to Holdex account
+      <br />
+      <a href="/about">Learn more</a>
+    </p>
+
+  </div>
+</div>
